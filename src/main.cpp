6c5c2296e6509981db1df61fb4d3145fc9d9293c
PCBMoreira2 <58518350+PCBMoreira2@users.noreply.github.com>
2024-06-12 00:59:59 -0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <ESP32Servo.h>
+
+constexpr int escPin = 21;
+constexpr int potPin = 4;
+
+Servo esc = Servo();
+
+void setup() {
+	Serial.begin(9600);
+	esc.attach(escPin, 1000, 2000);
+}
+
+void loop() {
+	int potValue = analogRead(potPin);
+	esc.write(map(potValue, 0, 4095, 0, 180));
+	Serial.println(esc.read());
+}
