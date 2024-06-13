cf991fac06938808fbc6a726f7ff96e74731bf31
Pedro Moreira <pcbmoreira@id.uff.br>
2024-06-13 16:52:19 -0300

    Criado a lógica de ativação dos motores.

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,48 @@
-/*
-   Gamepad module provides three different mode namely Digital, JoyStick and Accerleometer.
-
-   You can reduce the size of library compiled by enabling only those modules that you want to
-   use. For this first define CUSTOM_SETTINGS followed by defining INCLUDE_modulename.
-
-   Explore more on: https://thestempedia.com/docs/dabble/game-pad-module/
-*/
-
 #include <Arduino.h>
 #include <ESP32Servo.h>
-
-#define CUSTOM_SETTINGS
-#define INCLUDE_GAMEPAD_MODULE
 #include <DabbleESP32.h>
 
 const int escPin1 = GPIO_NUM_18;
 const int escPin2 = GPIO_NUM_19;
+
 Servo esc1 = Servo();
 Servo esc2 = Servo();
 
 void setup() {
   	// put your setup code here, to run once:
-  	Serial.begin(115200);      // make sure your Serial Monitor is also set at this baud rate.
-  	Dabble.begin("MyEsp32");       //set bluetooth name of your device
+  	Serial.begin(115200); 
+
 	esc1.attach(escPin1, 1000, 2000);
 	esc2.attach(escPin2, 1000, 2000);
 }
 
 void loop() {
-  	Dabble.processInput();             //this function is used to refresh data obtained from smartphone.Hence calling this function is mandatory in order to get data properly from your mobile.
-	// int radius = GamePad.getRadius();
-	// float vet = GamePad.getx_axis();
-	// float vetE;
-	// float vetD;
-	// int escE;
-	// int escD;
-	// if(vet > 0){
-	// 	vetE = vet /7.0f;
-	// 	vetD = (7-vet) / 7.0f;
-	// }else{
-	// 	vetE = (7-vet) / 7.0f;
-	// 	vetD = vet / 7.0f;
-	// }
-	// escE = radius * vetE;
-	// escD = radius * vetD;
-	// esc1.write(map(escE, 0, 7, 0, 180));
-	// esc2.write(map(escD, 0, 7, 0, 180));
+  	Dabble.processInput();
+
+	// Raio representa o multiplicador de velocidade dos dois motores
+	int radius = GamePad.getRadius();
+
+	// vet representa o eixo X do analógico (-7 a 7)
+	float vet = GamePad.getx_axis();
+
+	float vetE; // Quantos porcento o motor E está ligado
+	float vetD; // Quantos porcento o motor D está ligado
+	
+	if(vet > 0){
+		vetD = (7-vet)/7.0f;
+		vetE = 1;
+	}else if(vet < 0){
+		vetD = 1;
+		vetE = (7-vet)/7.0f;
+	}else{
+		vetD = 1;
+		vetE = 1;
+	}
+
+	int escE = radius * vetE;
+	int escD = radius * vetD;
+
+	// Envia o PWM para o ESC do motor (0 a 180)
+	esc1.write(map(escE, 0, 7, 0, 180));
+	esc2.write(map(escD, 0, 7, 0, 180));
 }
